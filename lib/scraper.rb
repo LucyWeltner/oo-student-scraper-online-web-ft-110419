47d024ec34f54f5de9f57469a9e214b89252c3e0
Lucy Weltner <lucyiweltner@gmail.com>
2019-12-02 00:13:21 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,27 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = Nokogiri::HTML(open(index_url))
+    student_info = html.css(".student-card")
+    students = []
+    student_info.each do |student|
+      student_hash = {}
+      student_hash[:name] = student.css("h4").text
+      student_hash[:location] = student.css("p").text
+      student_hash[:profile_url] = student.css("a")[0]["href"]
+      students << student_hash
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = Nokogiri::HTML(open(profile_url))
   end
 
 end
 
+Scraper.scrape_index_page("https://learn-co-curriculum.github.io/student-scraper-test-page/")
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,27 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+      @@all << self
+    end
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_info|
+      student_hash = {}
+      student_hash[:name] = student_info.css("h4").text
+      student_hash[:location] = student_info.css("p").text
+      Student.new(student_hash)
+    end
   end
 
-  def add_student_attributes(attributes_hash)
+  # def add_student_attributes(attributes_hash)
     
-  end
+  # end
 
   def self.all
-    
+    @@all
   end
 end
 
